868a5c81bf04cef6ee639b85c9a5dfae18e2343c
Abhishek Gupta <ag528927@gmail.com>
2020-04-18 21:01:11 +0530

    Update mongoose.config.ts

--- config/mongoose.config.ts
+++ config/mongoose.config.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-let dev_db_url = 'mongodb+srv://Abhishek:Abhishek@abhishek-cluster-jxsrb.mongodb.net/final_year_project_db?retryWrites=true&w=majority';
+let dev_db_url = 'mongodb+srv://*******:*******@abhishek-cluster-jxsrb.mongodb.net/final_year_project_db?retryWrites=true&w=majority';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true }).then(() => {
